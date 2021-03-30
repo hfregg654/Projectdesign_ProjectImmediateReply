4fa0151be60aee2a2d336e98c47e116aa8850311
wade <wade10431@gmail.com>
2021-03-30 18:27:21 +0900

    授權碼完成,亂數分組初版

--- ProjectImmediateReply/ProjectImmediateReply/Utility/RandomTool.cs
+++ ProjectImmediateReply/ProjectImmediateReply/Utility/RandomTool.cs
@@ -8,38 +8,44 @@ namespace ProjectImmediateReply.Utility
     public class RandomTool
     {
         /// <summary>
-        /// 創建授權碼,回傳值為"0000,0000,0000,0000"為一組的集合
+        /// 創建授權碼,回傳值為16位數字為一組的集合
         /// </summary>
         /// <param name="num">授權碼組數</param>
         /// <param name="Pri">傳入Session中的權限值,認證用</param>
         /// <returns></returns>
         public static List<string> CreateLicence(int num, string Pri)
         {
-            //判斷身分是否為管理者,不是救回傳null
+            //判斷身分是否為管理者,不是則回傳null
             if (Pri != "Manager")
                 return null;
-            //定義亂數種子  定義數字每4位為一個小組,每4組為一個授權碼,共16碼  另外定義計數器及重複檢查器
+            //定義亂數種子    
             Random random = new Random();
-            ushort licence_cell;
+            //定義數字每16位為一組授權碼
+            byte licence_cell;
             List<ushort> licencelist = new List<ushort>();
             List<string> licence = new List<string>();
-            uint counter = 0;
+            //另外定義計數器及重複檢查器
+            byte counter = 0;
             bool isrepeat;
             //嘗試抓取錯誤,有錯誤即回傳null及錯誤訊息
             try
             {
+                //重複創建不重複的授權碼,直到到達預定的數量
                 do
                 {
                     //重複檢查器初始化
                     isrepeat = false;
-                    //將亂數取得的4位數
-                    licence_cell = Convert.ToUInt16(random.Next(1000, 9999));
+                    //將亂數取得的0~9之數字放進集合中
+                    licence_cell = Convert.ToByte(random.Next(0, 10));
                     licencelist.Add(licence_cell);
-                    counter++;
-                    if (counter % 4 == 0)
+                    counter++;//計數器+1
+                    //每16位就將其合併成為一組授權碼
+                    if (counter % 16 == 0)
                     {
-                        counter = 0;
-                        licence.Add(string.Join(",", licencelist));
+                        counter = 0;//計數器歸零
+                        //將16位數字連接成一組授權碼
+                        licence.Add(string.Join("", licencelist));
+                        //檢查授權碼集合內是否有重複的授權碼,若有則將當前授權碼移除重新創建一組
                         for (int i = 0; i < licence.Count; i++)
                         {
                             for (int j = i + 1; j < licence.Count; j++)
@@ -54,6 +60,7 @@ namespace ProjectImmediateReply.Utility
                             if (isrepeat)
                                 break;
                         }
+                        //小授權碼集合清空
                         licencelist.Clear();
                     }
                 } while (licence.Count != num);
@@ -67,5 +74,95 @@ namespace ProjectImmediateReply.Utility
 
         }
 
+        public static List<string> RandomAssign(List<string> assignpeople,List<string> assignporject, string Pri)
+        {
+            if (Pri != "Manager")
+                return null;
+            Random random = new Random();
+            byte Team_cell;
+            byte projectnum;
+            List<string> people = new List<string>();
+            List<string> team = new List<string>();
+            List<string> _class = new List<string>();
+            if (assignpeople.Count % 4 == 0)
+            {
+                ushort peoplenum = Convert.ToUInt16(assignpeople.Count / 4);
+                foreach (string item in assignpeople)
+                {
+                    people.Add(item);
+                }
+                do
+                {
+                    Team_cell = Convert.ToByte(random.Next(0, people.Count));
+                    team.Add(people.ToArray()[Team_cell]);
+                    people.RemoveAt(Team_cell);
+                    if (team.Count % peoplenum == 0)
+                    {
+                        _class.Add(string.Join(",", team));
+                        team.Clear();
+                    }
+                } while (_class.Count != 4);
+                return _class;
+            }
+            else
+            {
+                ushort peoplelonly = Convert.ToUInt16(assignpeople.Count % 4);
+                ushort peoplenum = Convert.ToUInt16((assignpeople.Count - (peoplelonly)) / 4);
+                foreach (string item in assignpeople)
+                {
+                    people.Add(item);
+                }
+                do
+                {
+                    Team_cell = Convert.ToByte(random.Next(0, people.Count));
+                    team.Add(people.ToArray()[Team_cell]);
+                    people.RemoveAt(Team_cell);
+
+                    if (team.Count % peoplenum == 0)
+                    {
+                        if (peoplelonly != 0)
+                        {
+                            if (random.Next(2) == 0)
+                            {
+                                peoplelonly--;
+                                projectnum = Convert.ToByte(random.Next(assignporject.Count));
+                                Team_cell = Convert.ToByte(random.Next(0, people.Count));
+                                team.Add(people.ToArray()[Team_cell]);
+                                people.RemoveAt(Team_cell);
+                                _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
+                                assignporject.RemoveAt(projectnum);
+                                team.Clear();
+                            }
+                            else if (peoplelonly < _class.Count)
+                            {
+                                peoplelonly--;
+                                projectnum = Convert.ToByte(random.Next(assignporject.Count));
+                                Team_cell = Convert.ToByte(random.Next(0, people.Count));
+                                team.Add(people.ToArray()[Team_cell]);
+                                people.RemoveAt(Team_cell);
+                                _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
+                                assignporject.RemoveAt(projectnum);
+                                team.Clear();
+                            }
+                            else
+                            {
+                                projectnum = Convert.ToByte(random.Next(assignporject.Count));
+                                _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
+                                assignporject.RemoveAt(projectnum);
+                                team.Clear();
+                            }
+                        }
+                        else
+                        {
+                            projectnum = Convert.ToByte(random.Next(assignporject.Count));
+                            _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
+                            assignporject.RemoveAt(projectnum);
+                            team.Clear();
+                        }
+                    }
+                } while (_class.Count != 4);
+                return _class;
+            }
+        }
     }
 }
--- ProjectImmediateReply/ProjectImmediateReply/WebForm1.aspx.cs
+++ ProjectImmediateReply/ProjectImmediateReply/WebForm1.aspx.cs
@@ -11,13 +11,22 @@ namespace ProjectImmediateReply
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            List<string> people = new List<string>()
+            {
+                "陳彥瑋","鍾學武","饒文傑","林可偉","歐紀安","翁秉寬","郭家榮","胡冠傑","徐丞邑",
+                "葉韋豪","張瑋芝","吳遠哲","連秋堂"
+            };
+            List<string> project = new List<string>()
+            {
+                "財務系統","無人機管理系統","專案及時回覆系統","選課系統"
+            };
             DateTime start = DateTime.Now;
-            foreach (string item in Utility.RandomTool.CreateLicence(10, "Manager").ToArray())
+            foreach (string item in Utility.RandomTool.RandomAssign(people, project ,"Manager").ToArray())
             {
                 Response.Write($"{item}<br/>");
             }
             DateTime end = DateTime.Now;
-            Response.Write((end-start).ToString());
+            Response.Write((end - start).ToString());
         }
     }
 }
