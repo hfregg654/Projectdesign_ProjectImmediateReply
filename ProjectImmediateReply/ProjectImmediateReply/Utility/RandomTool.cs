0cd92946a69e09c93c8a9c39c997ae8167ee0166
wade <wade10431@gmail.com>
2021-03-31 14:00:30 +0900

    隨機分組工具完成

--- ProjectImmediateReply/ProjectImmediateReply/Utility/RandomTool.cs
+++ ProjectImmediateReply/ProjectImmediateReply/Utility/RandomTool.cs
@@ -73,102 +73,131 @@ namespace ProjectImmediateReply.Utility
             }
 
         }
+
+
         /// <summary>
-        /// 亂數將專案及小組分配為4組,回傳值為集合第一項為專案,後續為組員的集合
+        /// 將隨機人員加入小組集合
+        /// </summary>
+        /// <param name="people">當前剩餘人員之集合</param>
+        /// <param name="team">當前未滿人之小組集合</param>
+        private static void AssignMember(List<string> assignpeople, List<string> team)
+        {
+            //定義亂數種子並定義數字儲存被抽到的人員在集合中的號碼
+            Random random = new Random();
+            byte Team_cell = Convert.ToByte(random.Next(0, assignpeople.Count));
+            //將該人員放入小組集合中並將其移除於人員集合
+            team.Add(assignpeople.ToArray()[Team_cell]);
+            assignpeople.RemoveAt(Team_cell);
+        }
+        /// <summary>
+        /// 將隨機專案加入小組集合並將其加入班級集合
+        /// </summary>
+        /// <param name="assignporject">當前剩餘專案之集合</param>
+        /// <param name="team">當前已滿人之小組集合</param>
+        /// <param name="_class">當前未滿組之班級集合</param>
+        private static void AssignProject_AssignTeam(List<string> assignporject, List<string> team, List<string> _class)
+        {
+            //定義亂數種子並定義數字儲存被抽到的專案在集合中的號碼
+            Random random = new Random();
+            byte projectnum = Convert.ToByte(random.Next(assignporject.Count));
+            //將該專案放入及小組集合一同放入班級集合中並將被抽出專案移除於專案集合
+            _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
+            assignporject.RemoveAt(projectnum);
+            //清空小組集合
+            team.Clear();
+        }
+        /// <summary>
+        /// 亂數將專案及小組分配為4組,回傳值為第一項資料為專案,後續為組員的集合
         /// </summary>
         /// <param name="assignpeople">當前班級所有人的集合</param>
         /// <param name="assignporject">當前所有專案的集合</param>
         /// <param name="Pri">傳入Session中的權限值,認證用</param>
         /// <returns></returns>
-        public static List<string> RandomAssign(List<string> assignpeople,List<string> assignporject, string Pri)
+        public static List<string> RandomAssign(List<string> assignpeople, List<string> assignporject, string Pri)
         {
+            //判斷身分是否為管理者,不是則回傳null
             if (Pri != "Manager")
                 return null;
+            //定義亂數種子並定義"小組集合","班級集合"
             Random random = new Random();
-            byte Team_cell;
-            byte projectnum;
-            List<string> people = new List<string>();
             List<string> team = new List<string>();
             List<string> _class = new List<string>();
-            if (assignpeople.Count % 4 == 0)
+            //嘗試抓取錯誤,有錯誤即回傳null及錯誤訊息
+            try
             {
-                ushort peoplenum = Convert.ToUInt16(assignpeople.Count / 4);
-                foreach (string item in assignpeople)
+                //若班級人數為4的倍數
+                if (assignpeople.Count % 4 == 0)
                 {
-                    people.Add(item);
-                }
-                do
-                {
-                    Team_cell = Convert.ToByte(random.Next(0, people.Count));
-                    team.Add(people.ToArray()[Team_cell]);
-                    people.RemoveAt(Team_cell);
-                    if (team.Count % peoplenum == 0)
+                    //定義數字計算每組人數
+                    ushort peoplenum = Convert.ToUInt16(assignpeople.Count / 4);
+                    //重複執行分配直到4組完成
+                    do
                     {
-                        _class.Add(string.Join(",", team));
-                        team.Clear();
-                    }
-                } while (_class.Count != 4);
-                return _class;
-            }
-            else
-            {
-                ushort peoplelonly = Convert.ToUInt16(assignpeople.Count % 4);
-                ushort peoplenum = Convert.ToUInt16((assignpeople.Count - (peoplelonly)) / 4);
-                foreach (string item in assignpeople)
-                {
-                    people.Add(item);
+                        //分配人員
+                        AssignMember(assignpeople, team);
+                        //若小組內人數到達每組所需人數則分配專案並加入班級
+                        if (team.Count % peoplenum == 0)
+                            AssignProject_AssignTeam(assignporject, team, _class);
+                    } while (_class.Count != 4);
+                    //回傳班級集合
+                    return _class;
                 }
-                do
+                //若班級人數不是4的倍數
+                else
                 {
-                    Team_cell = Convert.ToByte(random.Next(0, people.Count));
-                    team.Add(people.ToArray()[Team_cell]);
-                    people.RemoveAt(Team_cell);
-
-                    if (team.Count % peoplenum == 0)
+                    //定義數字計算多出的人數及每組人數
+                    ushort peoplelonly = Convert.ToUInt16(assignpeople.Count % 4);
+                    ushort peoplenum = Convert.ToUInt16((assignpeople.Count - (peoplelonly)) / 4);
+                    //重複執行分配直到4組完成
+                    do
                     {
-                        if (peoplelonly != 0)
+                        //分配人員
+                        AssignMember(assignpeople, team);
+                        //若小組內人數到達每組所需人數
+                        if (team.Count % peoplenum == 0)
                         {
-                            if (random.Next(2) == 0)
+                            //若多出的人數不為0
+                            if (peoplelonly != 0)
                             {
-                                peoplelonly--;
-                                projectnum = Convert.ToByte(random.Next(assignporject.Count));
-                                Team_cell = Convert.ToByte(random.Next(0, people.Count));
-                                team.Add(people.ToArray()[Team_cell]);
-                                people.RemoveAt(Team_cell);
-                                _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
-                                assignporject.RemoveAt(projectnum);
-                                team.Clear();
-                            }
-                            else if (peoplelonly < _class.Count)
-                            {
-                                peoplelonly--;
-                                projectnum = Convert.ToByte(random.Next(assignporject.Count));
-                                Team_cell = Convert.ToByte(random.Next(0, people.Count));
-                                team.Add(people.ToArray()[Team_cell]);
-                                people.RemoveAt(Team_cell);
-                                _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
-                                assignporject.RemoveAt(projectnum);
-                                team.Clear();
+                                //先亂數決定是否在這組多加一人
+                                if (random.Next(2) == 0)
+                                {
+                                    //多出的人數-1,再抽一人加入該組並分配專案及加入班級
+                                    peoplelonly--;
+                                    AssignMember(assignpeople, team);
+                                    AssignProject_AssignTeam(assignporject, team, _class);
+                                }
+                                //若多出的人數已經比剩餘的組數少了
+                                else if (peoplelonly < _class.Count)
+                                {
+                                    //多出的人數-1,再抽一人加入該組並分配專案及加入班級
+                                    peoplelonly--;
+                                    AssignMember(assignpeople, team);
+                                    AssignProject_AssignTeam(assignporject, team, _class);
+                                }
+                                else
+                                    //其餘則直接分配專案並加入班級
+                                    AssignProject_AssignTeam(assignporject, team, _class);
                             }
                             else
-                            {
-                                projectnum = Convert.ToByte(random.Next(assignporject.Count));
-                                _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
-                                assignporject.RemoveAt(projectnum);
-                                team.Clear();
-                            }
+                                //其餘則直接分配專案並加入班級
+                                AssignProject_AssignTeam(assignporject, team, _class);
                         }
-                        else
-                        {
-                            projectnum = Convert.ToByte(random.Next(assignporject.Count));
-                            _class.Add($"{assignporject.ToArray()[projectnum]},{string.Join(",", team)}");
-                            assignporject.RemoveAt(projectnum);
-                            team.Clear();
-                        }
-                    }
-                } while (_class.Count != 4);
-                return _class;
+                    } while (_class.Count != 4);
+                    //回傳班級集合
+                    return _class;
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Current.Response.Write($"請聯繫開發人員。錯誤訊息：{ex}");
+                return null;
             }
         }
+
+
+
+
+
     }
 }
