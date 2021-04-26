ac8db7a6d5ee45870811507b4d15544e0bd0d473
wade <wade10431@gmail.com>
2021-04-26 12:36:27 +0900

    認可

--- ProjectImmediateReply/ProjectImmediateReply/ImmediateReplyInSide.Master.designer.cs
+++ ProjectImmediateReply/ProjectImmediateReply/ImmediateReplyInSide.Master.designer.cs
@@ -33,6 +33,24 @@ namespace ProjectImmediateReply
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// HiddenFieldSessionPri 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HiddenFieldSessionPri;
+
+        /// <summary>
+        /// HiddenFieldSessionMail 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HiddenFieldSessionMail;
+
+        /// <summary>
         /// ucLeft 控制項。
         /// </summary>
         /// <remarks>
